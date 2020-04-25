5a33de262442cd3e573d619909e5c57863ad4f38
Randy <rtruong60@gmail.com>
2020-04-25 14:38:00 -0400

    basic structure created with css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './styles/app.module.css'
+import {About, Contact, Footer, Header, Portfolio, Resume, Testimonials} from './components'
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label}>something</label>
+      <Header/>
+      <About/>
+      <Resume/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
